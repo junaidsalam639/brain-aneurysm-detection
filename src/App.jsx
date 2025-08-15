3e0f02d5dd5d15547c30c504016503d8aca7ba7c
Muhammad Junaid <121503655+junaidsalam639@users.noreply.github.com>
2025-08-15 22:04:32 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import { BrowserRouter } from "react-router"
-import { useEffect } from "react"
 import AppRoutes from "./routes"
-import { useDispatch } from "react-redux"
 
 function App() {
-const dispatch = useDispatch();
-
-  useEffect(() => {
-    const handleBeforeUnload = () => {
-      dispatch(clearAuth());
-    };
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [dispatch]);
-  
   return (
     <>
       <BrowserRouter>
