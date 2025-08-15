13b194637d7f14402074104d3f9f7eb127e7931e
Muhammad Junaid <121503655+junaidsalam639@users.noreply.github.com>
2025-08-15 22:15:30 +0500

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -5,10 +5,11 @@ import scanIdReducer from './scanIdSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
+import storageSession from "redux-persist/lib/storage/session";
 
 const persistConfig = {
     key: 'root',
-    storage,
+    storage: storageSession,
     whitelist: ['auth', 'scanId']
 };
 
